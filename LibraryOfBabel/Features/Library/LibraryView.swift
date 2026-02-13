47668e01545d9b6225352342579ae60614dfb83b
GrimProg <vavilovila01@gmail.com>
2026-02-13 14:23:22 +0600

    Сделал роутер и переделал боковое меню

--- LibraryOfBabel/Features/Library/LibraryView.swift
+++ LibraryOfBabel/Features/Library/LibraryView.swift
@@ -7,12 +7,8 @@
 
 import SwiftUI
 
-struct Library: View {
+struct LibraryView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-}
-
-#Preview {
-    Library()
 }
--- LibraryOfBabel/Router/MacRootView.swift
+++ LibraryOfBabel/Router/MacRootView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct MacRootView: View {
+    @StateObject var router = AppRouter()
+    
     var body: some View {
         NavigationSplitView {
-            
+            List(selection: $router.selectedRoute) {
+                NavigationLink(value: AppRoute.home) {
+                    Label("Главная", systemImage: "home")
+                }
+                NavigationLink(value: AppRoute.library) {
+                    Label("Библиотека", systemImage: "circle")
+                }
+                NavigationLink(value: AppRoute.settings) {
+                    Label("Настройки", systemImage: "circle")
+                }
+            }
         } detail: {
-            
+            switch router.selectedRoute {
+            case .home:
+                HomeView()
+            case .library:
+                LibraryView()
+            case .settings:
+                SettingsView()
+            default:
+                EmptyView()
+            }
         }
     }
 }
